70b7bc1c2fd89d4111b1df65f5f531899ce01667
Jaswal1p <Jaswal1p@yahoo.com>
2022-03-28 14:18:54 -0400

    deleted all comments related codes to restart from lesson 18.3

--- routes/api/pizza-routes.js
+++ routes/api/pizza-routes.js
@@ -1,19 +1,20 @@
 const router = require('express').Router();
+
 const {
-  getAllPizza,
-  getPizzaById,
-  createPizza,
-  updatePizza,
-  deletePizza
+    getAllPizza,
+    getPizzaById,
+    createPizza,
+    updatePizza,
+    deletePizza
 } = require('../../controllers/pizza-controller');
 
-// /api/pizzas
+// Set up GET all and POST at /api/pizzas
 router
   .route('/')
   .get(getAllPizza)
   .post(createPizza);
 
-// /api/pizzas/:id
+// Set up GET one, PUT, and DELETE at /api/pizzas/:id
 router
   .route('/:id')
   .get(getPizzaById)
